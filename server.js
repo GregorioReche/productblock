bbe4191a4fe6cbb6cbc2a39b5591079a1114f10e
Gregorio Reche <gregorioreche@Gregorios-MacBook-Air.local>
2025-09-29 13:59:49 -0300

    Se agrega variable en server para puerto.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+const port = 4000;
 app.use('/', express.static('dist'));
-app.listen(process.env.PORT || 4000, function () {console.log('Example app listening on port 4000!');});
+app.listen(process.env.PORT || port, function () {console.log(`Example app listening on port ${port}!`);});
 
